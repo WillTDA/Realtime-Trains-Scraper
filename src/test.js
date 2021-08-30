d15488f944036db9b293b1ba1f81cdcac3b4d415
WillTDA <62032600+WillTDA@users.noreply.github.com>
2021-08-30 21:46:14 +0100

    Update Test

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 const realtimeTrains = require("./index");
 
 (async () => {
-    let trainData = await realtimeTrains.getTrains("KGX", 1);
-    console.log(JSON.stringify(trainData, null, 2));
+    let trainData = await realtimeTrains.getTrains("KGX", 10);
+    console.log(trainData);
     console.log(`Returned ${trainData.length}/10 Results Requested from Target Station! (London King's Cross) ${trainData.length < 10 ? "(The Timetable for the Current Hour was Smaller than the Requested Result Count)" : ""}`);
 })();
